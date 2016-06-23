9a15859475f56a6cb637d638099436baebe214d1
Shinji.Yoshida <yoshida.shinji.gm@gmail.com>
2016-06-23 16:42:29 +0900

    SpriteResizingBar

---
+++ Assets/Scripts/LightweightBar/Scripts/SpriteResizingBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LightweightBar {
+	public class SpriteResizingBar : MonoBehaviour {
+		[SerializeField] BarModel barModel;
+		[SerializeField] Transform bar;
+
+		public void ChangeParameters (float minValue, float maxValue, float currentValue) {
+			barModel.ChangeParameters (minValue, maxValue, currentValue);
+			UpdateView ();
+		}
+
+		public void ChangeCurrentValue (float value) {
+			barModel.ChangeCurrentValue (value);
+			UpdateView ();
+		}
+
+		protected void OnValidate() {
+			barModel.FixParameter();
+			FixParameter();
+			UpdateView();
+		}
+
+		void FixParameter() {
+		}
+
+		void UpdateView () {
+			bar.localScale = new Vector3(barModel.NormalizedValue, 1, 1);
+		}
+	}
+}
